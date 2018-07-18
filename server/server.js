5aee00cee76d273a2bcc2b2b0605286ffc29fa48
Matt Beyl <matthew.beyl@gmail.com>
2018-07-18 16:27:13 -0500

    initial commit: server will spin up and client JS works. vendors sourced

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('server/public'));
+
+app.listen(PORT, function(){
+    console.log('App running on port ', PORT);
+})
